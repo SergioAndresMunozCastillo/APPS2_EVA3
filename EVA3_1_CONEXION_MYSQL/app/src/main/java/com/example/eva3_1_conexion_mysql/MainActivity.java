6d3c06d61778a09e77117305422554eefe938967
Sergio Andres Munoz Castillo <43544215+SergioAndresMunozCastillo@users.noreply.github.com>
2020-06-03 10:25:45 -0600

    Practica 1: conexion Mysql

---
+++ EVA3_1_CONEXION_MYSQL/app/src/main/java/com/example/eva3_1_conexion_mysql/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.eva3_1_conexion_mysql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lstVwActors;
+    ArrayList<JSONObject> miListaJSON = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lstVwActors = findViewById(R.id.lstVwActors);
+        new MiClaseConexion().execute("http://192.168.1.75:3000/actors");
+    }
+
+    class MiClaseConexion extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            String sRuta = strings[0];
+            String sResu = null;
+            try{
+                URL ruta = new URL(sRuta);
+                HttpURLConnection http = (HttpURLConnection)ruta.openConnection();
+                if(http.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    InputStreamReader inputStreamReader = new InputStreamReader(http.getInputStream());
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                }
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sResu;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s != null){
+                try{
+                    JSONArray jsonArray = new JSONArray(s);
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        miListaJSON.add(jsonObject);
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
